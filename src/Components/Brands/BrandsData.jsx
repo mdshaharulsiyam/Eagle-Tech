ce3434ea0e2aec158d9f21923f93d1c0e9076a49
shaharul siyam <shaharulsiyam0273@.com>
2023-10-18 15:39:37 +0600

    advantage section completed

--- src/Components/Brands/BrandsData.jsx
+++ src/Components/Brands/BrandsData.jsx
@@ -11,8 +11,8 @@ const BrandsData = ({singlebrand}) => {
     <div style={style} className='max-w-sm relative transition-all overflow-hidden rounded hover:scale-110 cursor-pointer' >
       <img className='lg:w-64 w-64  md:w-64 h-56 object-cover' src={img} alt="" />
       <div id='serviceCardcaption' className=' absolute w-full h-full top-[60%] left-0 bg-black bg-opacity-70 text-white pt-2'>
-        <h4>{brand}</h4>
-        <Link to={`/ServiceDetails/${id}`}><button className='font-semibold text-base italic mt-5 bg-[#CD979A] p-1 px-7 rounded'>see products..</button></Link>
+        <h4 className='text-xl tracking-[1px] italic font-bold'>{brand}</h4>
+        <Link to={`/ServiceDetails/${id}`}><button className='font-semibold text-base italic mt-2 bg-[#CD979A] p-1 px-7 rounded'>see products..</button></Link>
       </div>
     </div>
   )
--- src/Components/OurAdvantages/OurAdvantages.jsx
+++ src/Components/OurAdvantages/OurAdvantages.jsx
@@ -1,17 +1,46 @@
-import React from 'react'
-
+import { TbShoppingCartDiscount } from 'react-icons/tb';
+import { FaShippingFast,FaHandshake } from 'react-icons/fa';
+import { RiCustomerService2Fill} from 'react-icons/ri';
+import { FcShipped} from 'react-icons/fc';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 const OurAdvantages = () => {
-    const style = {
-        boxShadow: "rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px"
-    }
     return (
-        <div className='container mx-auto'>
-            <h2 className=' uppercase text-3xl text-center font-bold'>Our Advantages</h2>
-            <div className='flex flex-wrap justify-center items-center gap-7 py-10'>
-                <div style={style} className='max-w-sm lg:w-96 w-96  md:w-80 h-80 transition-all overflow-hidden rounded hover:scale-110 cursor-pointer' >
-                    <h2></h2>
-                </div>
-            </div>
+        <div className='container mx-auto my-10'>
+            <h2 className=' uppercase text-3xl text-center font-bold py-4 pb-16'>Our Advantages</h2>
+       
+            <Swiper
+                spaceBetween={50}
+                slidesPerView={3}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+            >
+                <SwiperSlide>  <div className='text-center box-border p-2 rounded-lg bg-base-300 pb-10'>
+                    <TbShoppingCartDiscount className='mx-auto text-8xl'></TbShoppingCartDiscount>
+                    <h4 className='text-xl font-bold italic pb-2'>Exclusive Deals & Discounts</h4>
+                    <p className='text-xs md:text-base'>Elevate your tech shopping experience with our Exclusive Deals & Discounts. Immerse yourself in a world of special offers and unparalleled savings, transforming every purchase into a rewarding and cost-effective journey through cutting-edge technology.</p>
+                </div></SwiperSlide>
+                <SwiperSlide>  <div className='text-center box-border p-2 rounded-lg bg-base-300 pb-10'>
+                    <FaShippingFast className='mx-auto text-8xl'></FaShippingFast>
+                    <h4 className='text-xl font-bold italic pb-2'>Fast & Secure Shipping</h4>
+                    <p className='text-xs md:text-base'>Enjoy the assurance of swift and secure shipping at Eagle Tech. Our commitment to delivering your tech products with speed and care ensures they reach you in optimal condition. Experience the convenience of reliable and efficient delivery services that prioritize the safety of your purchases.</p>
+                </div></SwiperSlide>
+                <SwiperSlide>  <div className='text-center box-border p-2 rounded-lg bg-base-300 pb-10'>
+                    <RiCustomerService2Fill className='mx-auto text-8xl'></RiCustomerService2Fill>
+                    <h4 className='text-xl font-bold italic pb-2'>Customer-Centric Service</h4>
+                    <p className='text-xs md:text-base'>At Eagle Tech, our Customer-Centric Service revolves around your satisfaction. Experience dedicated support tailored to meet all your tech needs. Our commitment goes beyond selling products; it extends to fostering a relationship built on trust, reliability, and exceptional service</p>
+                </div></SwiperSlide>
+                <SwiperSlide>  <div className='text-center box-border p-2 rounded-lg bg-base-300 pb-10'>
+                    <FaHandshake className='mx-auto text-8xl'></FaHandshake>
+                    <h4 className='text-xl font-bold italic pb-2'>Quality Assurance</h4>
+                    <p className='text-xs md:text-base'>Rest easy with Eagle Tech's unwavering commitment to Quality Assurance. We guarantee that every tech product you choose is reliable, durable, and upholds the highest standards of quality. Trust in our assurance for a seamless tech experience that exceeds expectations</p>
+                </div></SwiperSlide>
+                <SwiperSlide>  <div className='text-center box-border p-2 rounded-lg bg-base-300 pb-10'>
+                    <FcShipped className='mx-auto text-8xl'></FcShipped>
+                    <h4 className='text-xl font-bold italic pb-2'>User-Friendly Shopping</h4>
+                    <p className='text-xs md:text-base'>Navigate our website effortlessly for a User-Friendly Shopping experience. Immerse yourself in a seamless and enjoyable journey through our curated tech offerings. Explore with ease, discover with joy, and redefine your tech lifestyle with our intuitive and user-centric online platform.</p>
+                </div></SwiperSlide>
+            </Swiper>
         </div>
     )
 }
