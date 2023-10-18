e4e0f281ffff5d0b5a9f2fec79c15d0f3cf16cc4
shaharul siyam <shaharulsiyam0273@.com>
2023-10-18 10:01:09 +0600

    naveber completed

--- src/Components/Header/TopHeader/TopHeader.jsx
+++ src/Components/Header/TopHeader/TopHeader.jsx
@@ -6,9 +6,9 @@ const TopHeader = () => {
     return (
         <div className='bg-red-400 py-3 text-white'>
             <div className='container mx-auto flex justify-between items-center'>
-                <div className='flex justify-between items-center gap-4 font-medium'>
-                    <a className='flex justify-between items-center gap-1' href="tel:+880156602****"><BsFillTelephoneFill /> +880156602****</a>
-                    <a className='flex justify-between items-center gap-1' href="mailto:epictechspot@gmail.com"><AiOutlineMail />epictechspot@gmail.com</a>
+                <div className='sm:flex justify-between items-center gap-4 font-medium'>
+                    <a className='flex justify-start items-center gap-1' href="tel:+880156602****"><BsFillTelephoneFill /> +8801500000000</a>
+                    <a className='flex justify-start items-center gap-1' href="mailto:epictechspot@gmail.com"><AiOutlineMail />epictechspot@gmail.com</a>
                 </div>
                 <div className='flex text-2xl justify-end items-center gap-2'>
                     <a href="https://www.facebook.com/" target='_blank'><FaFacebook /></a>
--- src/Pages/Root/Root.jsx
+++ src/Pages/Root/Root.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import TopHeader from '../../Components/Header/TopHeader/TopHeader'
+import Navber from '../../Components/Header/Navber/Navber'
+
 
 const Root = () => {
     return (
         <div>
             <TopHeader></TopHeader>
+            <Navber></Navber>
             <Outlet></Outlet>
         </div>
     )
