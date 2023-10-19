395d39c3074b6390040f0434652c281ac7f72019
shaharul siyam <shaharulsiyam0273@.com>
2023-10-19 21:07:53 +0600

    project completed

--- src/Context/Context.jsx
+++ src/Context/Context.jsx
@@ -9,7 +9,7 @@ const Context = ({ children }) => {
   const [loading, setloading] = useState(true)
   const [currentuser, setcurrentuser] = useState(null)
   const [currentuserphoto, setcurrentuserphoto] = useState(null)
-  const host = "http://localhost:5000"
+  const host = "https://epic-tech-spot-backend-rixsqqax9-mdshaharulsiyams-projects.vercel.app"
   const signupuser = (password, email, username) => {
     setloading(true)
     createUserWithEmailAndPassword(auth, email, password)
