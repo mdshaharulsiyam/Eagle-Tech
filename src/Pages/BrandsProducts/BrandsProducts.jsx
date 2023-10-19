7af3b611b9d26d6292738153d6fac9d77ed1e1c8
shaharul siyam <shaharulsiyam0273@.com>
2023-10-19 14:41:10 +0600

    product page loadin aded

--- src/Pages/BrandsProducts/BrandsProducts.jsx
+++ src/Pages/BrandsProducts/BrandsProducts.jsx
@@ -10,30 +10,38 @@ const BrandsProducts = () => {
   const { host } = useContext(EagleTechSpotContext)
   const parems = useParams()
   const { brand } = parems
-
+  const [loadin, setloading] = useState(true)
   const [productdata, setproductdata] = useState([]);
   console.log(productdata);
   useEffect(() => {
-    fetch(`${host}/addpost/${brand}`).then((res) => res.json()).then((data) => setproductdata(data))
+    setloading(true)
+    const fetchdata = async () => {
+      const res = await fetch(`${host}/addpost/${brand}`)
+      const data = await res.json()
+      setproductdata(data)
+      setloading(false)
+    }
+    fetchdata()
   }, [])
-
+  if (loadin) {
+    return <div className='min-h-[400px]'><span className="loading absolute top-[50%] left-[50%] -translate-y-[50%] -translate-x-[50%] z-10 text-center w-20 loading-spinner text-error"></span></div>
+  }
   return (
-
-    (productdata.length <= 0 )? <h4 className='flex justify-center items-center text-4xl text-red-400 min-h-[400px]'>No products founds <BiBlock className='ml-3'></BiBlock> </h4> : <div>
-    <Carousel>
-      {
-      productdata.slice(0,3).map(Product => <Slider key={Product._id} Product={Product}></Slider>)
-     }
-    </Carousel>
-    <div className='container mx-auto text-center'>
-      <h2 id='servicesectionTitle' className='text-5xl font-semibold italic text-[#FF6969] py-10'>Products</h2>
-      <div className='flex flex-wrap justify-center items-center gap-7 py-10'>
+    (productdata.length <= 0) ? <h4 className='flex justify-center items-center text-4xl text-red-400 min-h-[400px]'>No products founds <BiBlock className='ml-3'></BiBlock> </h4> : <div>
+      <Carousel>
         {
-          productdata.map(Product => <Products key={Product._id} Product={Product}></Products>)
+          productdata.slice(0, 3).map(Product => <Slider key={Product._id} Product={Product}></Slider>)
         }
+      </Carousel>
+      <div className='container mx-auto text-center'>
+        <h2 id='servicesectionTitle' className='text-5xl font-semibold italic text-[#FF6969] py-10'>Products</h2>
+        <div className='flex flex-wrap justify-center items-center gap-7 py-10'>
+          {
+            productdata.map(Product => <Products key={Product._id} Product={Product}></Products>)
+          }
+        </div>
       </div>
     </div>
-  </div>
   )
 }
 
